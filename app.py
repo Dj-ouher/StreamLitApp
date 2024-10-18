ecefda682a40f71a100f3f9b4003824e825d5d78
Djouher Mansouri <djouher.mansouri1@gmail.com>
2024-10-18 16:33:19 +0200

    Init Repo

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write ("Hello World")
+
+st.snow()
+st.balloons()
+st.write("This is a Website")
+st.markdown("## Title")
+st.markdown("## Title")
+st.button("Press",icon="✅")
